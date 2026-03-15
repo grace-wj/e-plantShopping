c5b8e5efc1298884a0b9b2718f89db50e648b811
Grace Juan <grace.juan@c3.ai>
2026-03-15 14:43:03 -0700

    set up the global store

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// The Provider component from the react-redux library enables all components to access the Redux store
+import { Provider } from 'react-redux';
+// The Redux store holds the application's state, using the reducer defined in CartSlice.jsx
+import store from './store.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap App with Provider and pass the store so all components can access and interact with global state */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
